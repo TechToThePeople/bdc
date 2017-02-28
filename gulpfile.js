ba3c54d6332f9fd934207146d1395bccabd78e03
Xavier Dutoit <github@tttp.eu>
2017-02-28 23:25:26 +0100

    update with choropleth

--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,21 @@ var cleanCSS = require('gulp-clean-css');
 
 var DEST = './dist/';
 var jsfiles = [
-'node_modules/dc/dc.js',
+'dist/js/polyfill.js',
 'node_modules/d3/d3.js',
+'node_modules/d3-queue/build/d3-queue.js',
 'node_modules/crossfilter/crossfilter.js',
-'node_modules/boostrap/dist/js/boostrap.js',
+'node_modules/dc/dc.js',
 'node_modules/jquery/dist/jquery.js',
+'node_modules/boostrap/dist/js/boostrap.js',
+'node_modules/topojson/dist/topojson.js',
 
 ];
-var cssfiles = ['node_modules/bootstrap/dist/css/bootstrap.css','node_modules/bootstrap/dist/css/bootstrap-theme.css','node_modules/dc/dc.css'];
+var cssfiles = [
+'node_modules/bootstrap/dist/css/bootstrap.css',
+'node_modules/bootstrap/dist/css/bootstrap-theme.css',
+'node_modules/dc/dc.css',
+];
 //node_modules/bootstrap/dist/css/bootstrap
 //node_modules/jquery/dist/jquery.js
 
@@ -28,16 +35,17 @@ gulp.task('copy', function() {
 });
 
 
-gulp.task('minify-css', function() {
-  return gulp.src(DEST+ 'css/*.css')
+gulp.task('minify-css', ['copy'], function() {
+//  return gulp.src(DEST+ 'css/*.css')
+  return gulp.src(cssfiles)
     .pipe(concat('style.css'))
     .pipe(cleanCSS({compatibility: 'ie9'}))
     .pipe(gulp.dest(DEST));
 });
 
 gulp.task('minify-js', ['copy'],function() {
-  return gulp.src(DEST+'js/*.js')
-    // This will output the non-minified version
+//gulp.src(DEST+'js/*.js')
+return gulp.src(jsfiles)
     .pipe(concat('all.js'))
     .pipe(gulp.dest(DEST))
     // This will minify and rename to foo.min.js
